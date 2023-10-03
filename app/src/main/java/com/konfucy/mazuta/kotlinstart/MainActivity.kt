a6eb011ccffd8acd49b396cd02f7b98b29e8878b
Ragvalod <konfucy@gmail.com>
2023-10-03 10:31:30 +0300

    Initial commit

---
+++ app/src/main/java/com/konfucy/mazuta/kotlinstart/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.konfucy.mazuta.kotlinstart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
